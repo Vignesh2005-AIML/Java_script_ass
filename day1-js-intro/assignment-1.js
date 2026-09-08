6ad55182533f4367df5b0e54593deee4de990ec5
Vignesh N <natarajanvignesh38@gmail.com>
2026-09-08 20:40:26 +0530

    first commit

---
+++ day1-js-intro/assignment-1.js
@@ -0,0 +1,9 @@
+
+console.log("hello world")
+
+const name="vignesh"
+let age = 21
+var city = "chennai"
+
+console.log(`Hi i am ${name},my age is ${age} and i am  from ${city}`);
+
---
+++ day1-js-intro/assignment-2.js
@@ -0,0 +1,27 @@
+let name = "Vignesh";
+const age = 22;
+
+let city = "Chennai";
+const student = true;
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(student)
+
+
+let number = 100;
+let text = "Hello";
+let decimal = 10.5;
+let nothing = null;
+let notDefined;
+
+
+console.log(typeof name);        
+console.log(typeof age);         
+console.log(typeof city);        
+console.log(typeof isStudent);   
+console.log(typeof number);      
+console.log(typeof text);        
+console.log(typeof decimal); 
+console.log(typeof nothing);     
+console.log(typeof notDefined);  
